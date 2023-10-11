247f125e3e955b32822ff78c776ad09d33f535dc
jajijujang <ahmadfauzanarifhakim@gmail.com>
2023-10-11 07:30:29 +0700

    perubahan1

---
+++ resources/views/halaman/biodata.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>New Account</h1>
+    <h2>Sign UpForm</h2>
+    <form action="">
+        <label>first name</label><br>
+        <input type="text" name="name"><br><br>
+        <label>last name</label><br>
+        <input type="text" name="name"><br><br>
+        <label>gender</label><br>
+        <input type="radio">man</br>
+        <input type="radio">women</br>
+        <input type="radio">other</br><br>
+        <label>nationality:</label><input type="text">
+        <input type="submit" value="">
+    </form>
+</body>
+</html>
